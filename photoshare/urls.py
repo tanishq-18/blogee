217fdf6974e3d59719688f79778d2544c90dbf8b
Tanishq <tanishqsolanki30@gmail.com>
2022-07-15 17:00:52 +0530

    few things removed

--- photoshare/urls.py
+++ photoshare/urls.py
@@ -8,7 +8,6 @@ from django.conf import settings
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('feed.urls')),
-	path('users/', user_views.users_list, name='users_list'),
 	path('users/<slug>/', user_views.profile_view, name='profile_view'),
 	path('edit-profile/', user_views.edit_profile, name='edit_profile'),
 	path('my-profile/', user_views.my_profile, name='my_profile'),
@@ -16,11 +15,7 @@ urlpatterns = [
 	path('register/', user_views.register, name='register'),
         path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
         path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-        path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-        path('password-reset/done/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-        path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-        path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-]
+        ]
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -51,34 +51,11 @@ def users_list(request):
 def profile_view(request, slug):
 	p = Profile.objects.filter(slug=slug).first()
 	u = p.user
-	sent_friend_requests = FriendRequest.objects.filter(from_user=p.user)
-	rec_friend_requests = FriendRequest.objects.filter(to_user=p.user)
 	user_posts = Post.objects.filter(user_name=u)
-
-	friends = p.friends.all()
-
-	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
-
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=p.user)) == 1:
-				button_status = 'friend_request_sent'
-
-		# if we have recieved a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
 
 	context = {
 		'u': u,
-		'button_status': button_status,
-		'friends_list': friends,
-		'sent_friend_requests': sent_friend_requests,
-		'rec_friend_requests': rec_friend_requests,
-		'post_count': user_posts.count
+	    'post_count': user_posts.count
 	}
 
 	return render(request, "users/profile.html", context)
@@ -118,31 +95,13 @@ def edit_profile(request):
 def my_profile(request):
 	p = request.user.profile
 	you = p.user
-	sent_friend_requests = FriendRequest.objects.filter(from_user=you)
-	rec_friend_requests = FriendRequest.objects.filter(to_user=you)
+
 	user_posts = Post.objects.filter(user_name=you)
-	friends = p.friends.all()
 
-	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
-
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=you)) == 1:
-				button_status = 'friend_request_sent'
-
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
 
 	context = {
 		'u': you,
-		'button_status': button_status,
-		'friends_list': friends,
-		'sent_friend_requests': sent_friend_requests,
-		'rec_friend_requests': rec_friend_requests,
+
 		'post_count': user_posts.count
 	}
 
